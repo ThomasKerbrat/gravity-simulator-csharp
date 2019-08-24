8f2831e2359f573d5692b5c9d68fe546cc5d9dc9
Thomas Kerbrat <kerbrat@outlook.com>
2019-08-24 15:27:05 +0200

    Give particles in the random seed an initial velocity

--- BodySeedStrategies/RandomSeedStrategy.cs
+++ BodySeedStrategies/RandomSeedStrategy.cs
@@ -10,11 +10,13 @@ namespace gravity_simulator_csharp
 		public Body NextBody(Universe universe)
 		{
 			const uint amplitude = 1500;
+			const float velocity = 2;
+			double tetha = random.NextDouble() * 2 * Math.PI;
 
 			return new Body(
 				mass: 1e12f,
 				position: new Vector2((float)((random.NextDouble() - 0.5) * amplitude), (float)((random.NextDouble() - 0.5) * amplitude)),
-				velocity: Vector2.Zero,
+				velocity: new Vector2((float)(Math.Cos(tetha) * velocity), (float)(Math.Sin(tetha) * velocity)),
 				acceleration: Vector2.Zero
 			);
 		}
